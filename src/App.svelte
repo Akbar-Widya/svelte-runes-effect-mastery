ead5df9152b08095eb3b10df1c4c4976a71afa53
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2026-02-07 23:09:26 +0700

    Svelte runes: effect.

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let size = $state(50);
+  let color = $state("#ff3e00");
+
+  let canvas;
+
+  $effect(() => {
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = color;
+    context.fillRect(0, 0, size, size);
+  })
+</script>
+
+<canvas bind:this={canvas} width="100" height="100"></canvas>
